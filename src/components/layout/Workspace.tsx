d0be77f0525a4e6869af30b0b961ce66cac218ef
web-dev-codi <cordiscobrian@gmail.com>
2026-05-06 16:20:57 +0200

    fix: use correct react-resizable-panels API (Group/Panel/Separator) instead of PanelGroup/PanelResizeHandle

--- src/components/layout/Workspace.tsx
+++ src/components/layout/Workspace.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+import { Group, Panel, Separator } from 'react-resizable-panels'
 import { Sidebar } from './Sidebar'
 import { LessonPanel } from '../lesson/LessonPanel'
 import { EditorPanel } from '../editor/EditorPanel'
@@ -13,25 +13,25 @@ export const Workspace: React.FC = () => {
 	return (
 		<div className={styles.workspace}>
 			<div className={styles.desktopLayout}>
-				<PanelGroup direction="horizontal">
+				<Group direction="horizontal">
 					<Panel defaultSize={15} minSize={10} maxSize={25}>
 						<aside className={styles.sidebar}>
 							<Sidebar />
 						</aside>
 					</Panel>
-					<PanelResizeHandle className="resizeHandle" />
+					<Separator className="resizeHandle" />
 					<Panel defaultSize={35} minSize={20} maxSize={50}>
 						<section className={styles.lessonPanel}>
 							<LessonPanel />
 						</section>
 					</Panel>
-					<PanelResizeHandle className="resizeHandle" />
+					<Separator className="resizeHandle" />
 					<Panel defaultSize={50} minSize={25}>
 						<div className={styles.rightColumn}>
 							<EditorPanel />
 						</div>
 					</Panel>
-				</PanelGroup>
+				</Group>
 			</div>
 
 			<div className={styles.mobileLayout}>
